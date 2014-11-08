e401a2ffd7945173074e50dccfd4a8cefb0f6873
Michael Rose <elementation@gmail.com>
2014-11-08 15:00:26 -0700

    Pagination: fixed to work with API client

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,9 @@ class HomeController < ApplicationController
 
   def search
     @search = params[:search]
-    @search_results = @@scryer.search(@search)
-    @results = Kaminari.paginate_array(@search_results.results, total_count: @search_results.hits).page(params[:page]).per(25)
+    @page = (params[:page] || '1').to_i
+    @search_results = @@scryer.search(@search, (@page-1)*25, 25)
+    @results = @search_results.results
+    @paginate = Kaminari.paginate_array([], total_count: @search_results.hits).page(@page).per(25)
   end
 end
