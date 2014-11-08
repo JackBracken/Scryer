1a9d90db6fc272cd310b2a7d0aea230630e2aaa2
Michael Rose <elementation@gmail.com>
2014-11-08 13:09:04 -0700

    Scryer supports pagination

--- lib/scryer/scryer_client.rb
+++ lib/scryer/scryer_client.rb
@@ -7,20 +7,25 @@ module Scryer
         faraday.request  :json             # form-encode POST params
         faraday.response :rashify                  # log requests to STDOUT
         faraday.response :json                  # log requests to STDOUT
+        faraday.response :logger                  # log requests to STDOUT
         faraday.use :instrumentation
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
       end
     end
 
-    def search(param_hash)
+    def search(param_hash, from = 0, max = 25)
       newHash = {}
       param_hash.each do |k,v|
         newHash[k.to_s.camelize(:lower)] = v
       end
 
+
       puts newHash
 
-      resp = @conn.post '/v1/search', newHash
+      resp = @conn.post do |req|
+        req.url '/v1/search', {:from => from, :max => max}
+        req.body = newHash
+      end
 
       resp.body
     end
