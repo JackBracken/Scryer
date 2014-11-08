aeee3f2033c17a71fec5f9de88cefb85935cb8cb
Michael Rose <elementation@gmail.com>
2014-11-08 11:58:29 -0700

    _story_result: dlp vote view

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,8 @@ module HomeHelper
   def extract_names(categories, join_char=' ')
     categories.collect { |c| c.name }.join(join_char)
   end
+
+  def vote(thread_data)
+    thread_data.votenum/thread_data.votetotal
+  end
 end
