277e54e964bf77e473768b8697de813207fb59c0
Michael Rose <elementation@gmail.com>
2014-11-09 03:43:11 -0700

    Taglines: more

--- config/initializers/taglines.rb
+++ config/initializers/taglines.rb
@@ -2,9 +2,11 @@ Rails.configuration.taglines = [
     'Fanfiction Search Engine',
     'Fanfiction Search Engine',
     'Fanfiction Search Engine',
+    'Fanfiction Search Engine',
     'Purveyors of slightly shoddy fiction.',
     'Guilty pleasures abound!',
     'We have what you\'re looking for. Probably.',
     'It\'s been written a thousand times already.',
     'That\'s original.',
+    'Enabler of questionable tastes.',
 ].freeze
