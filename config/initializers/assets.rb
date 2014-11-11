f964f80083a05dda3ababa80a92fb2d33f84c5b8
Michael Rose <elementation@gmail.com>
2014-11-10 18:33:37 -0700

    assets: loading search.css now

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,5 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 #Rails.application.config.assets.precompile += ['*.js', '*\.css', '**/*.js', '**/*\.css']
-Rails.application.config.assets.precompile += ['home.css', 'search.js', 'errors.css', 'errors.js']
+Rails.application.config.assets.precompile += ['search.css', 'search.js', 'errors.css', 'errors.js']
 
