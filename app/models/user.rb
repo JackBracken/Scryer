c7d8aa448e2c6090fbfb3ab73e1569218ab8320b
Michael Rose <elementation@gmail.com>
2014-11-11 01:47:35 -0700

    Rollbar: disabled in Prod. User: fixing deprecation warning

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable
-  establish_connection "#{Rails.env}_dlp"
+  establish_connection "#{Rails.env}_dlp".to_sym
 
   has_many :feedbacks
 
