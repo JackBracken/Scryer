749162861faa3cb7414fd38d439184a8da97a542
Michael Rose <elementation@gmail.com>
2014-11-14 02:42:10 -0700

    search_controller: character union cached

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,13 +7,12 @@ class SearchController < ApplicationController
   def index
     @fandom_list = Fandom.fandom_facets(224)
     @characters = if @fandoms.size > 1
-      $scryer.union_characters(@fandoms).collect do |c|
+      union_characters(@fandoms).collect do |c|
         Hashie::Mash.new(id: c.character_id, name:c.name)
       end
     else
       Character.where(:fandom_id => 224)
     end
-    puts @characters
   end
 
   def omni
@@ -28,7 +27,7 @@ class SearchController < ApplicationController
   end
 
   def characters
-    @characters = $scryer.union_characters(params[:fandom])
+    @characters = union_characters(params[:fandom])
 
     respond_to do |format|
       format.json { render json: @characters }
@@ -36,6 +35,15 @@ class SearchController < ApplicationController
   end
 
 private
+
+  def union_characters(fandoms)
+    # Stable cache keys
+    fandoms = fandoms.sort
+
+    Rails.cache.fetch("fandom_character_union_#{fandoms}", :expires_in => 24.hours) do
+      $scryer.union_characters(fandoms)
+    end
+  end
 
   def extract_search
     @search = params[:search]
