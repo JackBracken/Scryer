5c3e1f929392a05e50e0693905d247de59868aa3
Michael Rose <elementation@gmail.com>
2014-11-17 01:07:45 -0700

    Solving nil issue with char unions

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ActiveRecord::Base
   def self.for_fandoms(fandoms)
-    if fandoms.size > 1
+    if fandoms && fandoms.size > 1
       self.fandom_union(fandoms).collect do |c|
         c = Character.new(id: c.character_id, name:c.name)
         c.readonly!
