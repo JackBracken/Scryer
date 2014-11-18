2ca6a041618595da942992ba59414d8381bba7a1
Michael Rose <elementation@gmail.com>
2014-11-18 00:50:31 -0700

    Cleanup of App main

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :authorize
+  before_filter :authorize_profiler
   before_filter :maintenance
-
-  def authorize
-    if is_admin?
-      Rack::MiniProfiler.authorize_request
-    end
-  end
 
   def is_admin?
     user_signed_in? && current_user.is_admin?
   end
 
+  private
+
+  def authorize_profiler
+    Rack::MiniProfiler.authorize_request if is_admin?
+  end
+
   def maintenance
-    if Rails.configuration.maintenance && !is_admin?
-      render 'shared/maintenance', :status => 503
-    end
+    render 'shared/maintenance', :status => 503 if Rails.configuration.maintenance && !is_admin?
   end
 end
